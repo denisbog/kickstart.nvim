98524156037ea8c8852293e48b8c44a251ae354c
Denis <denis.boghiu@gmail.com>
2025-01-31 19:23:30 +0200

    custom keymaps

--- init.lua
+++ init.lua
@@ -929,7 +929,7 @@ require('lazy').setup({
     --
     --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    'nvim-treesitter/nvim-treesitter-context',
+    -- 'nvim-treesitter/nvim-treesitter-context',
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 
---
+++ lua/custom/plugins/colemak.lua
@@ -0,0 +1,7 @@
+return {
+  vim.keymap.set('n', 'n', 'j'),
+  vim.keymap.set('n', 'e', 'k'),
+  vim.keymap.set('n', 'm', 'h'),
+  vim.keymap.set('n', 'i', 'l'),
+  vim.keymap.set('n', 'k', 'i'),
+}
