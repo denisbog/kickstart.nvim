ccd89986e747d3fbf3c6c116d85938ebf3464eb1
Denis <denis.boghiu@gmail.com>
2025-01-31 20:06:29 +0200

    change binding for leap

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -1,7 +1,10 @@
 return {
   'ggandor/leap.nvim',
   config = function()
-    require('leap').create_default_mappings()
+    -- require('leap').create_default_mappings()
+    vim.keymap.set({ 'n', 'x', 'o' }, 'l', '<Plug>(leap-forward)')
+    vim.keymap.set({ 'n', 'x', 'o' }, 'L', '<Plug>(leap-backward)')
+    vim.keymap.set({ 'n', 'x', 'o' }, 'gl', '<Plug>(leap-from-window)')
   end,
   lazy = false,
 }
