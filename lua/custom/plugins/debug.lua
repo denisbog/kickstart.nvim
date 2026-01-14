aabe66c2b3a4bb88822a76e768ec9a6db30c4e38
Denis Boghiu <denis.boghiu.ext@infocert.it>
2026-01-14 10:23:22 +0200

    additional plugin

--- lua/custom/plugins/debug.lua
+++ lua/custom/plugins/debug.lua
@@ -84,6 +84,7 @@ return {
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         'codelldb',
+        'java-debug-adapter',
       },
     }
 
